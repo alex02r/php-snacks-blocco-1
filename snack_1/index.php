812c01698dd9effef7910393f63f0f9eaadbacff
alex02r <alex.giochi02@gmail.com>
2024-01-24 14:52:20 +0100

    [UPGRADE] Aggiunto array

--- snack_1/index.php
+++ snack_1/index.php
@@ -6,6 +6,27 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <?php 
+        $match = [
+            [
+                'casa' => 'Milano',
+                'ospite' => 'Treviso',
+                'punti_casa' => 60,
+                'punti_ospite' => 75
+            ],
+            [
+                'casa' => 'Venezia',
+                'ospite' => 'Sassari',
+                'punti_casa' => 83,
+                'punti_ospite' => 80
+            ],
+            [
+                'casa' => 'Roma',
+                'ospite' => 'Lazio',
+                'punti_casa' => 98,
+                'punti_ospite' => 104 
+            ]
+        ];
+    ?>
 </body>
 </html>
