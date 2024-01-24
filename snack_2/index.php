4981f7ae0277d3ec4b7ff735932eee020c753429
alex02r <alex.giochi02@gmail.com>
2024-01-24 15:09:36 +0100

    [UPGRADE] Aggiunta la form

--- snack_2/index.php
+++ snack_2/index.php
@@ -6,6 +6,20 @@
     <title>Snack 2</title>
 </head>
 <body>
+    <?php 
     
+    ?>
+    <form action="index.php" method="GET">
+        <label for="nome">Nome</label>
+        <input type="text" id="nome" name="name">
+
+        <label for="email">Email</label>
+        <input type="mail" id="email" name="mail">
+
+        <label for="eta">Età</label>
+        <input type="text" id="eta" name="age">
+
+        <button type="submit">Verifica</button>
+    </form>
 </body>
 </html>
